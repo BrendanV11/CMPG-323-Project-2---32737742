9bff7f6db43a68fbabf78867a5eb9222012a5c65
BrendanV11 <brendan.trexftw@gmail.com>
2022-09-06 15:06:18 +0200

    Added Authentication along with various classes
    Modified Controller
    
    Modified .gitignore
    
    Modified .json
    
    Modified startup.cs

---
+++ Authentication/UserRoles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2_API.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
+
+
+
