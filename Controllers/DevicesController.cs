3fe96831d9bf295c6a04e2f56f893e699e0dcb6a
BrendanV11 <brendan.trexftw@gmail.com>
2022-09-07 11:29:18 +0200

    Added patch methods

--- Controllers/DevicesController.cs
+++ Controllers/DevicesController.cs
@@ -121,5 +121,28 @@ namespace Project2_API.Controllers
         {
             return _context.Device.Any(e => e.DeviceId == id);
         }
+
+        [HttpPatch]
+        public async Task<ActionResult<Device>> PatchCategory(Device device)
+        {
+            _context.Device.Add(device);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DeviceExists(device.DeviceId))
+                {
+                    _context.Device.Update(device);
+                }
+                else
+                {
+                    return Conflict();
+                }
+            }
+
+            return CreatedAtAction("GetDevice", new { id = device.CategoryId }, device);
+        }
     }
 }
